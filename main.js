5177bbc23a8667cb8e9e0fc2ff99c0107c24bdc0
CandaceNia <98069665+CandaceNia@users.noreply.github.com>
2022-04-08 10:56:51 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+// Styling
+
+// Behavior
+fetch(https://github.com/chubin/wttr.in)
+.then((response) => response.json ())
+.then((json) => {
+    console.log(Here)
+})
+.catch((error) => {
+    console.log(error);
+});
+// Media Quiries
