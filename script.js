a49d7994c346b6e556b9d0b10cbab6bc04f96c29
arupay <arupay11374@gmail.com>
2022-04-10 11:52:16 -0400

    first article current wethaer complete

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const form = document.querySelector("form");
+const general = document.getElementById("general");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,7 +13,37 @@ const weatherSearch = (location) => {
       return response.json();
     })
     .then((result) => {
-      console.log(result);
+      const { temp_F } = result.current_condition[0];
+      const { areaName, country, region } = result.nearest_area[0];
+      const { chanceofrain, chanceofsunshine, chanceofsnow } =
+        result.weather[0].hourly[0];
+      console.log(chanceofrain);
+      const searchedTown = document.createElement("h2");
+      searchedTown.innerText = location;
+      const searchedArea = document.createElement("div");
+      searchedArea.innerHTML = "Nearest Area" + areaName[0].value;
+      const searchedRegion = document.createElement("div");
+      searchedRegion.innerHTML = "Region: " + region[0].value;
+      const searchedCountry = document.createElement("div");
+      searchedCountry.innerHTML = "Country: " + country[0].value;
+      const currentTemp = document.createElement("div");
+      currentTemp.innerHTML = "Currently: " + temp_F;
+      const sunshine = document.createElement("div");
+      sunshine.innerHTML = "Chance of Sunshine: " + chanceofsunshine;
+      const rain = document.createElement("div");
+      rain.innerHTML = "Chance of Rain: " + chanceofrain;
+      const snow = document.createElement("div");
+      snow.innerHTML = "Chance of Snow: " + chanceofsnow;
+      general.append(
+        searchedTown,
+        searchedArea,
+        searchedRegion,
+        searchedCountry,
+        currentTemp,
+        sunshine,
+        rain,
+        snow
+      );
     })
     .catch((error) => {
       console.log(error);
