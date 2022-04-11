fae84fee0fde9595023b38bfd029fff6eaa812f5
Edwin Codrington <edwincodrington@gmail.com>
2022-04-11 11:32:48 -0400

    Test1 complete

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 const header = document.querySelector("header");
 const form = document.querySelector("form");
 
+/** Adds the weather at given location */
 const parse_data = (file) => {
+	//See the text disappear from the search bar
+	const searchBar = document.querySelector("input.search-bar");
+	let temp = searchBar.textContent;
+	searchBar.textContent = "";
+	//See the name of the city that was searched as well as the area, region, country, and currently "feels like" temperature for that location.
+	const currentWeather = document.querySelector("article.current-weather");
+	let city = document.createElement("h3");
+	city.textContent = temp;
+	//See detailed information for the current day and the next two days below the main element.
+	//See the city name and "feels like" temperature show up in the aside element.
 	return undefined;
 };
 
-fetch(`wttr.in/NewYork?format=j1`)
-	.then((response) => response.json())
-	.then(parse_data)
-	.catch((error) => console.log(error));
+/** Event listener for "Get Weather" form */
+form.addEventListener("submit", (event) => {
+	let location = event.target.textContent; //"Melbourne"
+
+	fetch(`wttr.in/${location}?format=j1`) //wttr.in/Melbourne?format=j1
+		.then((response) => response.json())
+		.then(parse_data)
+		.catch((error) => console.log(error));
+});
