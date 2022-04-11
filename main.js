1c0ece917866e7aa7b83e116585974f624ad5466
named.josie <named.josie@gmail.com>
2022-04-10 21:39:42 -0400

    update app

---
+++ main.js
@@ -0,0 +1,16 @@
+// const BASE_URL = 'wttr.in';
+// const form = document.querySelector('form');
+// form.addEventListener('submit', (event) => {
+//   event.preventDefault();
+  
+// fetch(BASE_URL)
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((json) => {
+//     console.log(json)
+//   })
+//   .catch((ERROR) => {
+//     console.log(ERROR);
+//   });
+//  })
