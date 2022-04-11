7536312dd54ada876dcdd5ee435e7ea1587f529a
named.josie <named.josie@gmail.com>
2022-04-11 11:47:36 -0400

    update html file in app

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-// const BASE_URL = 'wttr.in';
+// const BASE_URL = 'https://wttr.in/Tokyo?format=j1';
 // const form = document.querySelector('form');
 // form.addEventListener('submit', (event) => {
 //   event.preventDefault();
-  
-// fetch(BASE_URL)
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((json) => {
-//     console.log(json)
-//   })
-//   .catch((ERROR) => {
-//     console.log(ERROR);
-//   });
-//  })
+
+//   fetch(BASE_URL)
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((json) => {
+//       console.log(json);
+//     })
+//     .catch((ERROR) => {
+//       console.log(ERROR);
+//     });
+// });
