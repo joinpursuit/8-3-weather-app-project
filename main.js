962d0ef87d11bb4653b9ee86710513f9790ded01
Edwin Codrington <edwincodrington@gmail.com>
2022-04-12 03:20:46 -0400

    Bone structure for js

--- main.js
+++ main.js
@@ -3,14 +3,17 @@ const form = document.querySelector("form");
 
 /** Adds the weather at given location */
 const parse_data = (file) => {
-	//See the text disappear from the search bar
+	//toggle hidden parts: "No previous searches" and "Choose a location..."
+	document.querySelectorAll("p.hidden").forEach((item) => item.classList.toggle("hidden"));
+
+	//TODO: See the text disappear from the search bar
 	const searchBar = document.querySelector("input.search-bar");
-	let temp = searchBar.textContent;
+	let searchQuery = searchBar.textContent;
 	searchBar.textContent = "";
-	//See the name of the city that was searched as well as the area, region, country, and currently "feels like" temperature for that location.
+	//TODO: See the name of the city that was searched as well as the area, region, country, and currently "feels like" temperature for that location.
 	const currentWeather = document.querySelector("article.current-weather");
 	let city = document.createElement("h3");
-	city.textContent = temp;
+	city.textContent = searchQuery;
 	currentWeather.append(city);
 
 	let area = document.createElement("p");
@@ -26,16 +29,79 @@ const parse_data = (file) => {
 	currentWeather.append(country);
 
 	let currently = document.createElement("p");
-	currently.innerHTML = `<strong>Currently:</strong> Feels like ${file.current_condition[0].FeelsLikeF}°F`;
+	currently.innerHTML = `<strong>Currently:</strong> Feels Like ${file.current_condition[0].FeelsLikeF}°F`;
 	currentWeather.append(currently);
-	//See detailed information for the current day and the next two days below the main element.
-	//See the city name and "feels like" temperature show up in the aside element.
+	//TODO: See detailed information for the current day and the next two days below the main element.
+	//TODO: THIS PROCESS CAN DEFINITELY BE LOOPED SOME WAY
+	//@mToday START
+	let today = document.querySelector("article.today-weather");
+	let todayText = document.createElement("h4");
+	todayText.textContent = "Today";
+	today.append(todayText);
+
+	let avgToday = document.createElement("p");
+	avgToday.innerHTML = `<strong>Average Temperature:</strong> ${file.weather[0].avgTempF}°F`; //for tomorrow, index 1; for day after, index 2
+	today.append(avgToday);
+
+	let maxToday = document.createElement("p");
+	maxToday.innerHTML = `<strong>Max Temperature:</strong> ${file.weather[0]}°F`; //file.weather[0].what?!?!???! is it heat index?
+	today.append(maxToday);
+
+	let minToday = document.createElement("p");
+	minToday.innerHTML = `<strong>Min Temperature:</strong> ${file.weather[0]}°F`; //file.weather[0].what should point to min weather. dew point?!
+	today.append(minToday);
+	//@Today END
+
+	//@Tomorrow START
+	let tomorrow = document.querySelector("article.tomorrow-weather");
+	let tomorrowText = document.createElement("h4");
+	tomorrowText.textContent = "Tomorrow";
+	tomorrow.append(tomorrowText);
+
+	let avgTomorrow = document.createElement("p");
+	avgTomorrow.innerHTML = `<strong>Average Temperature:</strong> ${file.weather[1].avgTempF}°F`;
+	tomorrow.append(avgTomorrow);
+
+	let maxTomorrow = document.createElement("p");
+	maxTomorrow.innerHTML = `<strong>Max Temperature:</strong> ${file.weather[1]}°F`; //file.weather[1].what?!?!???! is it heat index?
+	tomorrow.append(maxTomorrow);
+
+	let minTomorrow = document.createElement("p");
+	minTomorrow.innerHTML = `<strong>Min Temperature:</strong> ${file.weather[1]}°F`; //file.weather[0].what should point to min weather. dew point?!
+	tomorrow.append(minTomorrow);
+	//@Tomorrow END
+
+	//@Day-after-tomorrow START
+	let dayAfterTomorrow = document.querySelector("article.day-after-tomorrow-weather");
+	let dayAfterTomorrowText = document.createElement("h4");
+	dayAfterTomorrowText.textContent = "Day After Tomorrow";
+	dayAfterTomorrow.append(dayAfterTomorrowText);
+
+	let avgDayAfterTomorrow = document.createElement("p");
+	avgDayAfterTomorrow.innerHTML = `<strong>Average Temperature:</strong> ${file.weather[2].avgTempF}°F`;
+	dayAfterTomorrow.append(avgDayAfterTomorrow);
+
+	let maxDayAfterTomorrow = document.createElement("p");
+	maxDayAfterTomorrow.innerHTML = `<strong>Max Temperature:</strong> ${file.weather[2]}°F`; //file.weather[1].what?!?!???! is it heat index?
+	dayAfterTomorrow.append(maxDayAfterTomorrow);
+
+	let minDayAfterTomorrow = document.createElement("p");
+	minDayAfterTomorrow.innerHTML = `<strong>Min Temperature:</strong> ${file.weather[2]}°F`; //file.weather[0].what should point to min weather. dew point?!
+	dayAfterTomorrow.append(minDayAfterTomorrow);
+	//@Day-after-tomorrow END
+
+	//TODO:See the city name and "feels like" temperature show up in the aside element.
+	let previousSearchesList = document.querySelector("aside.weather-history ul");
+	//make list item
+	let listItem = document.createElement("li");
+	listItem.innerHTML = `${searchQuery} - ${file.current_condition[0].FeelsLikeF}°F`;
+	previousSearchesList.append(listItem); //TODO: make the Cityname a hyperlink
 };
 
 /** Event listener for "Get Weather" form */
 form.addEventListener("submit", (event) => {
 	event.preventDefault();
-	let location = event.target.value; //TODO: not passing proper location str
+	let location = event.target.text; //TODO: not passing proper location str
 
 	fetch(`https://wttr.in/${location}?format=j1`) //wttr.in/Melbourne?format=j1
 		.then((response) => response.json())
