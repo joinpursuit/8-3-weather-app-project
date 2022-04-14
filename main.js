dc6cd6de1fa68529881c7ed1dff057f8b31776c9
Lortiz528 <lortiztech@gmail.com>
2022-04-14 12:26:33 -0400

    5 full tests passed 19 total

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const base_url = 'https://wttr.in/';
 let weatherSearch = document.querySelector('.get-weather');
 let place = document.querySelector('#input-text');
 let widget = document.querySelector('#temp-conversion');
+let forecasts = document.querySelector('.forecasts');
 let h2 = document.querySelector('h2');
 let cityName;
 
@@ -11,6 +12,7 @@ weatherSearch.addEventListener('submit', (event) => {
   let cityName = place.value;
   let full_url = `${base_url}${cityName}?format=j1`;
   widget.hidden = false; // make the widget appear
+  forecasts.hidden = false;
 
   fetch(full_url)
     .then((response) => response.json())
@@ -25,23 +27,27 @@ weatherSearch.addEventListener('submit', (event) => {
       const region = document.querySelector('#Region');
       const country = document.querySelector('#Country');
       const currently = document.querySelector('#Currently');
-      let feeltemp = result.current_condition[0].FeelsLikeF;
       let sunshineChance = document.querySelector('#ChanceOfSunshine');
       let rainChance = document.querySelector('#ChanceOfRain');
       let snowChance = document.querySelector('#ChanceOfSnow');
+
+      let feeltemp = result.current_condition[0].FeelsLikeF;
       let chanceOfSunshine = Number(
         result.weather[0].hourly[0].chanceofsunshine
       );
       let chanceOfRain = Number(result.weather[0].hourly[0].chanceofrain);
       let chanceOfSnow = Number(result.weather[0].hourly[0].chanceofsnow);
       let areaName = result.nearest_area[0].areaName[0].value;
+      // console.log(result);
+      // console.log(cityName.toLowerCase());
+      // console.log(areaName.toLowerCase());
 
       // if nothing is inputted, use nearest area.
       if (!cityName) {
         cityName = areaName;
       }
       // if user input doesnt match, gives nearest area
-      if (cityName !== areaName) {
+      if (cityName.toLowerCase() !== areaName.toLowerCase()) {
         area.innerHTML = `<strong> Nearest Area: </strong> ${areaName}`;
         h2.textContent = cityName;
       } else {
@@ -56,6 +62,22 @@ weatherSearch.addEventListener('submit', (event) => {
       rainChance.innerHTML = `<strong>Chance of Rain:</strong> ${chanceOfRain}`;
       snowChance.innerHTML = `<strong>Chance of Snow:</strong> ${chanceOfSnow}`;
 
+      let today = document.querySelectorAll('#today p');
+      let tomorrow = document.querySelectorAll('#tomorrow p');
+      let dayAFterTom = document.querySelectorAll('#day-after-tomorrow p');
+
+      today[0].textContent = `Average temperature: ${result.weather[0].avgtempF}\u00B0F`;
+      today[1].textContent = `Max temperature: ${result.weather[0].maxtempF}\u00B0F`;
+      today[2].textContent = `Min temperature: ${result.weather[0].mintempF}\u00B0F`;
+
+      tomorrow[0].textContent = `Average temperature: ${result.weather[1].avgtempF}\u00B0F`;
+      tomorrow[1].textContent = `Max temperature: ${result.weather[1].maxtempF}\u00B0F`;
+      tomorrow[2].textContent = `Min temperature: ${result.weather[1].mintempF}\u00B0F`;
+
+      dayAFterTom[0].textContent = `Average temperature: ${result.weather[2].avgtempF}\u00B0F`;
+      dayAFterTom[1].textContent = `Max temperature: ${result.weather[2].maxtempF}\u00B0F`;
+      dayAFterTom[2].textContent = `Min temperature: ${result.weather[2].mintempF}\u00B0F`;
+
       let history = document.querySelector('ul');
       let previous = document.createElement('li');
       history.append(previous);
@@ -64,7 +86,6 @@ weatherSearch.addEventListener('submit', (event) => {
       // let a = document.querySelector('a');
       // a.addEventListener('click', (e) => {
       //   e.preventDefault();
-      
 
       //   // fetch url with cityname and populate article .current-weather
       // });
@@ -87,7 +108,19 @@ weatherSearch.addEventListener('submit', (event) => {
     .catch((error) => console.log(error));
 });
 
+// const forcast = ({ result }) => {
+//   today[0].textContent = `Average temperature: ${result.weather[0].avgtempF}\u00B0F`;
+//   today[1].textContent = `Max temperature: ${result.weather[0].maxtempF}\u00B0F`;
+//   today[2].textContent = `Min temperature: ${result.weather[0].mintempF}\u00B0F`;
 
+//   tomorrow[0].textContent = `Average temperature: ${result.weather[1].avgtempF}\u00B0F`;
+//   tomorrow[1].textContent = `Max temperature: ${result.weather[1].maxtempF}\u00B0F`;
+//   tomorrow[2].textContent = `Min temperature: ${result.weather[1].mintempF}\u00B0F`;
+
+//   dayAFterTom[0].textContent = `Average temperature: ${result.weather[2].avgtempF}\u00B0F`;
+//   dayAFterTom[1].textContent = `Max temperature: ${result.weather[2].maxtempF}\u00B0F`;
+//   dayAFterTom[2].textContent = `Min temperature: ${result.weather[2].mintempF}\u00B0F`;
+// };
 
 // function createErrorMessage(message) {
 //   const section = document.createElement('section');
@@ -95,40 +128,4 @@ weatherSearch.addEventListener('submit', (event) => {
 //   section.innerHTML = `<p>There was an error!</p><p class="message">${message}</p>`;
 
 //   return section;
-// }
-
-// function forcaster(json) {
-//   let todaysWeather = {
-//     date: "Today",
-//     avgTempF: json.weather[0].avgtempF,
-//     maxTempF: json.weather[0].maxtempF,
-//     minTempF: json.weather[0].mintempF,
-//   };
-//   let tomorrowWeather = {
-//     date: "Tomorrow",
-//     avgTempF: json.weather[1].avgtempF,
-//     maxTempF: json.weather[1].maxtempF,
-//     minTempF: json.weather[1].mintempF,
-//   };
-//   let dayAfterTomorrowWeather = {
-//     date: "Day After Tomorrow",
-//     avgTempF: json.weather[2].avgtempF,
-//     maxTempF: json.weather[2].maxtempF,
-//     minTempF: json.weather[2].mintempF,
-//   };
-
-//   let weatherData = {
-//     area,
-//     region,
-//     country,
-//     feeltemp,
-//     hourly,
-//     weatherOfThreeDays: [
-//       todaysWeather,
-//       tomorrowWeather,
-//       dayAfterTomorrowWeather,
-//     ],
-//   };
-
-//   return weatherData;
 // }
