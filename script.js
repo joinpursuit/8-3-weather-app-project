3778d6dd2dc34b43a07d12fdf44b300d68eebc7e
EmaleeSoto <emaleesoto@gmail.com>
2022-04-17 19:40:41 -0400

    Passed second test where weather data is retirevied from API and displayed

--- script.js
+++ script.js
@@ -26,7 +26,60 @@ form.addEventListener("submit", (event) => {
   //TODO: Create catch error message
   fetch(`http://wttr.in/${location}?format=j1`)
     .then((response) => response.json())
-    .then((json) => {})
+    .then((json) => {
+      mainArticleParagraph.remove();
+      mainArticle.append(createWeatherBlock(json, location));
+    })
     .catch((error) => {});
   document.getElementById("location").value = ""; //reset input field text value
 });
+
+function paragraphBuilder(object, heading) {
+  const newParagraph = document.createElement("p");
+  const nearestArea = object.nearest_area[0];
+  const currentWeather = object.current_condition[0];
+  if (heading === "Area") {
+    const city = nearestArea.areaName[0].value;
+    newParagraph.textContent = `${heading}: ${city}`;
+  } else if (heading === "Region") {
+    const city = nearestArea.region[0].value;
+    newParagraph.textContent = `${heading}: ${city}`;
+  } else if (heading === "Country") {
+    const city = nearestArea.country[0].value;
+    newParagraph.textContent = `${heading}: ${city}`;
+  } else if (heading === "Currently") {
+    newParagraph.textContent = `Currently: Feels like ${currentWeather.FeelsLikeF}°F`;
+  }
+  newParagraph.style.fontWeight = "bold";
+  newParagraph.style.fontSize = "large";
+  return newParagraph;
+}
+
+//Takes in object and string to create weather location block in Main
+function createWeatherBlock(object, titleOfSection) {
+  const newSection = document.createElement("section"); //creates a section that will later be added to main article
+
+  //create heading
+  const heading = document.createElement("h2");
+  heading.style.fontSize = "x-large";
+  heading.innerHTML = titleOfSection;
+  heading.style.color = "#54416d";
+
+  areaHeading = paragraphBuilder(object, "Area");
+
+  const regionHeading = paragraphBuilder(object, "Region");
+  const countryHeading = paragraphBuilder(object, "Country");
+  const currentWeatherInF = paragraphBuilder(object, "Currently");
+
+  newSection.append(
+    heading,
+    areaHeading,
+    regionHeading,
+    countryHeading,
+    currentWeatherInF
+  );
+  //TODO: Move to CSS
+  newSection.classList.add("fadein");
+
+  return newSection;
+}
