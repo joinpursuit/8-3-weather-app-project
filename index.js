00a2a622422d20ff1db5751078edcb12a2784ea0
bowersjuan <bowersjuan@gmail.com>
2022-04-17 06:19:48 -0400

    Update files

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ getWeatherForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   let city = event.target.search.value;
-  city = capitalizeAllFirstLetters(city);
+  city = format(city);
   // Formats location input to inlcuded in API Calls for unique locations, otherwise uses nearest location if city is an empty string
 
   let url;
   if (city === "undefined") {
     url = `${BASE_URL}/?${queryParam}=${queryParamValue}`;
-  } else if (city !== undefined) {
+  } else {
     url = `${BASE_URL}/${city}?${queryParam}=${queryParamValue}`;
   }
   // Creates a final URL to fetch with considering whether the user has inputted a value for the location form
@@ -29,7 +29,7 @@ getWeatherForm.addEventListener("submit", (event) => {
     })
     .then((response) => {
       console.log(response);
-      return displayAllInfo(response);
+      return displayAllInfo(response, city);
     })
     .catch((error) => {
       console.log(error);
@@ -38,12 +38,11 @@ getWeatherForm.addEventListener("submit", (event) => {
 
   if (city !== "undefined") {
     event.target.search.value = "";
-    event.target.search.placeholder = "";
   }
 });
 
 // Helper Functions
-function capitalizeAllFirstLetters(str) {
+function format(str) {
   str = str.split(" ");
   return str
     .map((element) => {
@@ -53,16 +52,20 @@ function capitalizeAllFirstLetters(str) {
 }
 
 function displayCurrentLocWeatherInfo(response) {
-  const locationRequestForm = document.querySelector("main form");
-  locationRequestForm.classList.add("hidden");
   const mainArticle = document.querySelector("main article");
   mainArticle.classList.remove("hidden");
   const mainAside = document.querySelector("main aside");
   mainAside.classList.remove("hidden");
+  const chooseLocRequest = document.querySelector("main p");
+  chooseLocRequest.classList.add("hidden");
   // Hides Location Request form and shows Information Relevant to Input Location
 
-  mainArticle.innerHTML = "Current Weather";
-  // Resets appended information
+  mainArticle.innerHTML = "";
+  // Resets appended information for new location selection
+
+  const currentWeatherHeading = document.createElement("h3");
+  currentWeatherHeading.innerText = "Current Weather";
+  mainArticle.append(currentWeatherHeading);
 
   const weatherIcon = document.createElement("img");
   weatherIcon.setAttribute("style", "display: block;");
@@ -74,22 +77,22 @@ function displayCurrentLocWeatherInfo(response) {
   // Appends Weather Icon
 
   const nearestArea = document.createElement("p");
-  nearestArea.innerText = `Nearest Area: ${response.nearest_area[0].areaName[0].value}`;
+  nearestArea.innerText = `Nearest Area:\n${response.nearest_area[0].areaName[0].value}`;
   mainArticle.append(nearestArea);
   // Appends Nearest Area
 
   const region = document.createElement("p");
-  region.innerText = `Region: ${response.nearest_area[0].region[0].value}`;
+  region.innerText = `Region:\n${response.nearest_area[0].region[0].value}`;
   mainArticle.append(region);
   // Appends Region
 
   const country = document.createElement("p");
-  country.innerText = `Country: ${response.nearest_area[0].country[0].value}`;
+  country.innerText = `Country:\n${response.nearest_area[0].country[0].value}`;
   mainArticle.append(country);
   // Appends Country
 
   const currentWeather = document.createElement("p");
-  let tempUnit = "C";
+  let tempUnit = "F";
   let tempUnitKey = `FeelsLike${tempUnit}`;
   currentWeather.innerText = `Currently: Feels like ${response.current_condition[0][tempUnitKey]}°${tempUnit}`;
   mainArticle.append(currentWeather);
@@ -114,31 +117,31 @@ function displayCurrentLocWeatherInfo(response) {
 function displayThreeDayForecast(response) {
   const forecastCards = document.querySelectorAll("main section aside article");
 
-  let tempUnit = "C";
+  let tempUnit = "F";
   let avgTempKey = `avgtemp${tempUnit}`;
   let maxTempKey = `maxtemp${tempUnit}`;
   let minTempKey = `mintemp${tempUnit}`;
 
-  forecastCards[0].innerHTML = "<strong>Today</strong>";
-  forecastCards[1].innerHTML = "<strong>Tomorrow</strong>";
-  forecastCards[2].innerHTML = "<strong>Day After Tomorrow</strong>";
+  forecastCards[0].innerHTML = "<h4>Today</h4>";
+  forecastCards[1].innerHTML = "<h4>Tomorrow</h4>";
+  forecastCards[2].innerHTML = "<h4>Day After Tomorrow</h4>";
 
   for (let i = 0; i < 3; i++) {
     forecastCards[i].setAttribute("class", "centerText");
 
     const avgWeatherInfo = document.createElement("p");
-    avgWeatherInfo.innerText = `Average Temperature: ${response.weather[i][avgTempKey]}°${tempUnit}`;
+    avgWeatherInfo.innerText = `Average: ${response.weather[i][avgTempKey]}°${tempUnit}`;
     avgWeatherInfo.setAttribute("class", "centerText");
     forecastCards[i].append(avgWeatherInfo);
     forecastCards[i].setAttribute("class", "centerText");
 
     const maxWeatherInfo = document.createElement("p");
-    maxWeatherInfo.innerText = `Max Temperature: ${response.weather[i][maxTempKey]}°${tempUnit}`;
+    maxWeatherInfo.innerText = `Max: ${response.weather[i][maxTempKey]}°${tempUnit}`;
     maxWeatherInfo.setAttribute("class", "centerText");
     forecastCards[i].append(maxWeatherInfo);
 
     const minWeatherInfo = document.createElement("p");
-    minWeatherInfo.innerText = `Min Temperature: ${response.weather[i][minTempKey]}°${tempUnit}`;
+    minWeatherInfo.innerText = `Min: ${response.weather[i][minTempKey]}°${tempUnit}`;
     minWeatherInfo.setAttribute("class", "centerText");
     forecastCards[i].append(minWeatherInfo);
   }
@@ -159,7 +162,26 @@ function createWeatherIcons(response) {
   }
 }
 
-function displayAllInfo(response) {
+function createsPreviousSearches(response, city) {
+  const tempPreviousSearchesFiller = document.querySelector("aside section p");
+  tempPreviousSearchesFiller.setAttribute("class", "hidden");
+
+  const previousSearchesList = document.querySelector("aside ul");
+  const searchedLocation = document.createElement("li");
+
+  if (city === "undefined") {
+    city = response.nearest_area[0].areaName[0].value;
+  } else {
+    city = city.replaceAll("&", " ");
+  }
+  console.log(response.nearest_area[0].areaName[0].value);
+  console.log(city);
+  searchedLocation.innerText = city;
+  previousSearchesList.append(searchedLocation);
+}
+
+function displayAllInfo(response, city) {
+  createsPreviousSearches(response, city);
   displayCurrentLocWeatherInfo(response);
   displayThreeDayForecast(response);
   createWeatherIcons(response);
