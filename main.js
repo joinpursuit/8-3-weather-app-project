2c2efc4df93fcfc6562019c7f4126b6b318022d9
BertCasale <bthebest55@gmail.com>
2023-01-03 21:57:49 -0500

    show search result data for today

--- main.js
+++ main.js
@@ -2,7 +2,13 @@
 //get the searches list element
 const searchedList = document.querySelector(".searches");
 
+//get the article element for the main results
+const searchResult = document.querySelector(".search-result");
 
+//get the aside element for the results of today, tomorrow, and the day after tomorrow
+const upcoming = document.querySelector(".upcoming")
+
+console.log(searchResult, searchedList)
 //get the form and add the submit event listener
 document.querySelector("form").addEventListener("submit",(event) => {
     //stop the page from refreshing
@@ -48,7 +54,12 @@ document.querySelector("form").addEventListener("submit",(event) => {
 
         // //place the results in the main .search-result article
 
-        //
+        //change the inner html of the search-result article element to the data
+        searchResult.innerHTML = `<h2>${searchLocation}</h2>
+        <p><b>Area:</b> ${result.nearest_area[0].areaName[0].value}</p>
+        <p><b>Region:</b> ${result.nearest_area[0].region[0].value}</p>
+        <p><b>Country:</b> ${result.nearest_area[0].country[0].value}</p>
+        <p><b>Currently:</b> Feels Like ${result.current_condition[0].FeelsLikeF}°F</p>`;
 
 
 
