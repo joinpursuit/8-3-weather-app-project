0c298fd60550885d4c7aa53083840ca6dc8c0641
BertCasale <bthebest55@gmail.com>
2023-01-03 20:18:32 -0500

    update css, comment a plan in main.js

--- main.js
+++ main.js
@@ -2,17 +2,30 @@
 document.querySelector("form").addEventListener("submit",(event) => {
     //stop the page from refreshing
     event.preventDefault();
-    
+
     //the link for the searches in json
     fetch(`https://wttr.in/${event.location.target.value}?format=j1`)
 
-    .then((result) => {
+    .then((res) => {
         //log a success message
         console.log("Fetch was successful")
         //convert from json
-        result.json()
+        res.json()
     })
 
-    .then((result) => result)
+    .then((result) => {
+        //place the link to the search in previous section
+
+        //place the results in the main .searched article
+
+        //create 3 articles with today, tomorrow, and day after tomorrow results
+
+        
+
+
+
+
+
+    })
 })
 
