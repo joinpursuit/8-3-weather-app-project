5184ba54ccfe460fe87abea664678e95beb0e012
Wilsantos1975 <Faustosantos@pursuit.org>
2023-01-04 19:26:27 -0500

    main data retrieved

--- index.js
+++ index.js
@@ -6,22 +6,20 @@ document.querySelector("form").addEventListener("submit", (event) => {
       return result.json();
     })
     .then((weather) => {
+      let article = document.querySelector("article");
+      article.innerHTML = `<h1 class="city">${weather.nearest_area[0].areaName[0].value}</h1>`;
 
-        let main = document.getElementsByClassName(".main");
-        main.article.innerHTML = weather.nearest_area[0].areaName[0].value
+      let p = document.createElement("p");
+      p.innerHTML = `<span class="details"><strong>Area:</strong> ${weather.nearest_area[0].region[0].value}</span>
+        <br> <span class="details"><strong>Country:</strong> ${weather.nearest_area[0].country[0].value}</span> 
+        <br> <span class="details"><strong>Region:</strong> ${weather.nearest_area[0].region[0].value}</span> 
+        <br> <span class="details"><strong>Currently feels like:</strong> ${weather.current_condition[0]["FeelsLikeF"]}</span>`;
 
-        // let h1 = `<h1>{weather.nearest_area[0].areaName[0].value}</h1>`;
-        // // document.main.article.innerHTML = h1
-        
-        // console.log(weather.nearest_area[0].areaName[0].value)
+      article.appendChild(p);
+
+      console.log(weather);
     });
 });
-
-
-
-
-
-
 
 // document.querySelector("form").addEventListener("submit", (event) => {
 //     event.preventDefault()
