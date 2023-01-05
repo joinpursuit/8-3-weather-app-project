7b8768f718e3b1ba89bcb93af4dfe6543d0cf648
Wilsantos1975 <Faustosantos@pursuit.org>
2023-01-04 22:18:48 -0500

    previous seraches functionality added

--- index.js
+++ index.js
@@ -1,33 +1,53 @@
 document.querySelector("form").addEventListener("submit", (event) => {
+
   event.preventDefault();
 
-  fetch(`https://v3.wttr.in/${event.target.location.value}?format=j1`)
+  let article = document.querySelector("article");
+  let ul = document.querySelector("ul");
+  let previous = document.querySelector(".previous");
+  let p = document.createElement("p");
+
+  fetch(`https://wttr.in/${event.target.location.value}?format=j1`)
     .then((result) => {
       return result.json();
     })
     .then((weather) => {
-      let article = document.querySelector("article");
       article.innerHTML = `<h1 class="city">${weather.nearest_area[0].areaName[0].value}</h1>`;
-
-      let p = document.createElement("p");
       p.innerHTML = `<span class="details"><strong>Area:</strong> ${weather.nearest_area[0].region[0].value}</span>
         <br> <span class="details"><strong>Country:</strong> ${weather.nearest_area[0].country[0].value}</span> 
         <br> <span class="details"><strong>Region:</strong> ${weather.nearest_area[0].region[0].value}</span> 
         <br> <span class="details"><strong>Currently feels like:</strong> ${weather.current_condition[0]["FeelsLikeF"]}</span>`;
 
       article.appendChild(p);
-
-      console.log(weather);
     });
+
+  // second fetch call for previous searches
+
+  fetch(`https://wttr.in/${event.target.location.value}?format=j1`)
+    .then((res) => {
+      return res.json();
+    })
+    .then((weather) => {
+      // previous.remove()
+      if (previous) {
+        previous.remove()
+        let li = document.createElement('li')
+        li.innerHTML= `${weather.nearest_area[0].areaName[0].value}`
+        ul.append(li)
+
+      } else {
+        let li = document.createElement('li')
+        li.innerHTML= `${weather.nearest_area[0].areaName[0].value}`
+        ul.append(li)
+      }
+      
+    });
+    
+    
 });
 
-// document.querySelector("form").addEventListener("submit", (event) => {
-//     event.preventDefault()
-//     console.log(`https://v3.wttr.in/${event.target.location.value}?format=j1`, "HELLO FROM THE OTHER SIDE")
-//     fetch(`https://v3.wttr.in/${event.target.location.value}?format=j1`).then(result => {
-//         console.log("Fetch was successful")
-//         return result.json()
-//     }).then(weather => {
-//         console.log(weather, "Finally properly formatted weather")
-//     })
-// });
+// let li = (document.createElement("li").innerHTML = `${event.target.location.value}`);
+
+// previous.innerHTML = li;
+
+//  ul.append(li)
