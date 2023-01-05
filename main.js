cb1197c2ea9738baea8b1add8adfd832221eda91
JenniferEinstein <jennifereinstein@gmail.com>
2023-01-04 22:22:41 -0500

    worked on conversion widget

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const ul=document.createElement("ul")
 //from Jose C (lines 1 and 2)
 
 const locationInput = document.querySelector("input") //from Paola
+let city = event.target.location.value; //from Evan
 
 document.querySelector("form").addEventListener("submit",(event) => {
     event.preventDefault()
@@ -21,13 +22,29 @@ fetch(`https://v3.wttr.in/${event.target.location.value}?format=j1`).then(result
     bigLocation.innerText = `${event.target.location.value}`
     mainArticle.append(bigLocation)
 
+    /* do each thing one by one before appending.
+
+    const bigLocationArea = document.querySelector
+    const bigLocationRegion
+    const bigLocationCountry
+    const bigLocationFeelsLike
+*/
     // ul.textContent = `Feels Like ${weather.current_condition[0].FeelsLikeC}*C`
     // article[0].append(ul)
     })
 });
 
 
+/* When the input field receives input, convert the value from fahrenheit to celsius */
+function temperatureConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputCelsius").innerHTML = (valNum-32) / 1.8;
+  }
 
+  function temperatureConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputFahrenheit").innerHTML=(valNum*1.8)+32;
+  }
 
 /* in class:
 document.querySelector("form").addEventListener("submit", (event) => {
