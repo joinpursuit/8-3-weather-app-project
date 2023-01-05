9650d34065c92a2879e3696953ebc9eb87edae60
JenniferEinstein <jennifereinstein@gmail.com>
2023-01-04 19:10:51 -0500

    Up to where everyone else was?

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
+const article = document.querySelectorAll("article")
+const ul=document.createElement("ul")
+//from Jose C (lines 1 and 2)
+
+const locationInput = document.querySelector("input") //from Paola
+
+document.querySelector("form").addEventListener("submit",(event) => {
+    event.preventDefault()
+    console.log(`https://v3.wttr.in/${event.target.location.value}?format=j1`, "Hello from the other side")
+
+fetch(`https://v3.wttr.in/${event.target.location.value}?format=j1`).then(result => {
+    console.log("Fetch was successful")
+    return result.json()
+}).then(weather =>{
+    console.log(weather)
+    ul.textContent=`Currently:`
+    ul.after(`Feels Like ${weather.current_condition[0].FeelsLikeC}*C`)
+
+
+
+    // ul.textContent = `Feels Like ${weather.current_condition[0].FeelsLikeC}*C`
+    // article[0].append(ul)
+    })
+});
+
+
+
+
+/* in class:
 document.querySelector("form").addEventListener("submit", (event) => {
     event.preventDefault()
     console.log(event, "Hello from the other side")
@@ -11,3 +40,4 @@ document.querySelector("form").addEventListener("submit", (event) => {
         console.log(weather, "Finally properly formatted weather")
     })
 })
+*/
