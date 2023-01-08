bcf63a8c3f4934d08f460f3c804ce588121487e1
Angel <EMAIL>
2023-01-08 16:08:32 -0500

    tempToday

--- weather.js
+++ weather.js
@@ -1,10 +1,75 @@
-document.querySelector("form").addEventListener("submit", (event) => {
-    event.preventDefault()
-    console.log(`https://v3.wttr.in/${event.target.location.value}?format=j1`, "Default not popping up, you're good")
-    fetch(`https://v3.wttr.in/${event.target.location.value}?format=j1`).then(result => {
-        console.log("Fetch was successful")
-        return result.json()
-    }).then(weather => {
-        console.log(weather, "Formatted weather")
+const Base_URL = "https://wttr.in/"
+const END_URL = '?format=j1'
+
+const form = document.querySelector('#location');
+const main = document.querySelector('main');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const weather = document.querySelector('#inputText');
+    const current = document.querySelector('#currentWeather')
+
+    let userLocationSubmit = weather.value
+
+    const url = `${Base_URL}${userLocationSubmit}${END_URL}`
+    form.reset();
+    fetch(url)
+    .then((res) => res.json())
+    .then((res2) => {
+        
+        const hiddenLocation = document.querySelector('#currentWeather')
+        hiddenLocation.innerHTML = `<h2>${userLocationSubmit}</h2>`
+
+        const area = res2.nearest_area[0].areaName[0].value;
+        const areaS = document.createElement('p');
+        areaS.innerHTML = `<strong>Nearest Area: </strong>${area}`;
+        current.append(areaS);
+
+        const region = res2.nearest_area[0].region[0].value;
+        const regionS = document.createElement('p');
+        regionS.innerHTML = `<strong>Region: </strong>${region}`;
+        current.append(regionS);
+
+        const country = res2.nearest_area[0].country[0].value;
+        const countries = document.createElement('p');
+        countries.innerHTML = `<strong>Country: </strong>${country}`;
+        current.append(countries);
+
+        const feelsLike = res2.current_condition[0].FeelsLikeF;
+        const feelsLike2 = document.createElement('p');
+        feelsLike2.innerHTML = `<strong>Currently: </strong>Feels Like ${feelsLike}°F`;
+        current.append(feelsLike2);
+
+        const sun = res2.weather[0].hourly[0].chanceOfSunshine;
+        const sunny = document.createElement('p');
+        sunny.innerHTML = `<strong>Chance of Sunshine: </strong>${sun}`;
+        current.append(sunny);
+
+        const rain = res2.weather[0].hourly[0].chanceOfRain;
+        const rainy = document.createElement('p');
+        rainy.innerHTML = `<strong>Chance of Rain: </strong>${rain}`;
+        current.append(rainy);
+
+        const snow =res2.weather[0].hourly[0].chanceOfSnow;
+        const snowy = document .createElement('p');
+        snowy.innerHTML = `<strong>Chance of Snow: </strong>${snow}`;
+        current.append(snowy);
+
+        const today = document.querySelector('#today');
+        today.innerHTML = `<h2>Today</h2>`
+        const avgTempToday = document.createElement('p');
+        const minTempToday = document.createElement('p');
+        const maxTempToday = document.createElement('p');
+        const avgTemp = res2.weather[0].avgtempF
+        avgTempToday.innerHTML = `<strong>Average Temperature: </strong>${avgTemp}°F`;
+        const minTemp = res2.weather[0].minTempF
+        minTempToday.innerHTML = `<strong>Min Temperature: </strong>${minTemp}°F`;
+        const maxTemp = res2.weather[0].maxtempF
+        maxTempToday.innerHTML = `<strong>Max Temperature: </strong>${maxTemp}°F`;
+
+        today.append(avgTempToday, minTempToday, maxTempToday);
+
+        const tomorrow = document.querySelector()
+
     })
-});
+})
