176e7820e373c4ba6c4722821212f5d0e6de1a09
SuperNinjaEv <evan0210@gmail.com>
2023-01-08 15:33:41 -0500

    Fully functional with all extras

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const BASE_URL = 'https://wttr.in/';
 const locationForm = document.querySelector('.locationForm');
-const locationWeather = document.querySelector('.location-weather h2');
+const locationWeather = document.querySelector('article');
+const weatherIcon = document.querySelector('img');
 
 // Event Listener for City Submit Search
 locationForm.addEventListener("submit", (event) => {
@@ -13,12 +14,16 @@ locationForm.addEventListener("submit", (event) => {
     fetchWeather(citySearched);
 })
 
+/**
+ * Function to FETCH weather info
+ *  Also creates previous search section
+ */
 function fetchWeather(city) {
     fetch(`${BASE_URL}${city}?format=j1`)
         .then((response) => response.json())
         .then((json) => {
             //Makes H2 in Main = to city searched, then invokes displayWeather function that I created to fill in data
-            locationWeather.innerText = city;
+            document.querySelector('article h2').innerText = city;
             console.log(json);
             displayWeather(json);
 
@@ -26,7 +31,11 @@ function fetchWeather(city) {
              * Previous Search History Creation
              */
             // Selecting and hiding message of no previous
-            document.querySelector('#noSearch').hidden = true;
+            let prev = document.querySelector('#noSearch');
+            if (prev) {
+                prev.remove();
+            }
+
             // Creating 2 new elements
             let prevSearch = document.createElement('li');
             let prevCity = document.createElement('a');
@@ -46,6 +55,7 @@ function fetchWeather(city) {
                 // PREPENDS the City a-tag to the prevSearch li-tag
                 // APPENDS the whole li-tag (that now contains the a-tag) to the previous search aside
                 // Selects to a-tag based on the city class and APPENDS the temp
+
                 prevSearch.classList = city
                 prevSearch.prepend(prevCity)
                 document.querySelector('#previous-searches').append(prevSearch)
@@ -56,65 +66,77 @@ function fetchWeather(city) {
             }
         })
         .catch((error) => console.log(error));
-        
-        function displayWeather(data) {
-            let { value: nearest } = data.nearest_area[0].areaName[0];
-            let { value: region } = data.nearest_area[0].region[0];
-            let { value: country } = data.nearest_area[0].country[0];
-            let { FeelsLikeF } = data.current_condition[0];
-            let { chanceofsunshine } = data.weather[0].hourly[4];
-            let { chanceofrain } = data.weather[0].hourly[4];
-            let { chanceofsnow } = data.weather[0].hourly[4];
-        
-            console.log(nearest, region, country, FeelsLikeF, chanceofsunshine, chanceofrain, chanceofsnow);
-        
-            if (city.toLowerCase() === nearest.toLowerCase()) {
-                document.querySelector(".nearest").innerHTML = `<b>Area:</b> ` + nearest;
-            } else {
-                document.querySelector(".nearest").innerHTML = `<b>Nearest Area:</b> ` + nearest;
-            }
-            document.querySelector(".region").innerHTML = `<b>Region:</b> ` + region;
-            document.querySelector(".country").innerHTML = `<b>Country:</b> ` + country;
-            document.querySelector(".currentTemp").innerHTML = `<b>Currently:</b> Feels Like ` + FeelsLikeF + ` &degF`;
-            document.querySelector(".sunChance").innerHTML = `<b>Chance of Sunshine:</b> ` + chanceofsunshine + `%`;
-            document.querySelector(".rainChance").innerHTML = `<b>Chance of Rain:</b> ` + chanceofrain + `%`;
-            document.querySelector(".snowChance").innerHTML = `<b>Chance of Snow:</b> ` + chanceofsnow + `%`;
-        
-            // Forecast structure creation
-            // let weatherToday = document.createElement('article');
-            // let weatherTomorrow = document.createElement('article');
-            // let weatherThreeDay = document.createElement('article');
-            // weatherToday.innerHTML = `<b>Today</b>`;
-            // weatherTomorrow.innerHTML = `<b>Tomorrow</b>`;
-            // weatherThreeDay.innerHTML = `<b>Day After Tomorrow</b>`;
-            // document.querySelector('.forecast').append(weatherToday, weatherTomorrow, weatherThreeDay)
-        
-            document.querySelector('.forecast').innerHTML = '';
-            const forecastDays = ['Today', 'Tomorrow', 'Day After Tomorrow'];
-        
-            // Loop for Forecast
-            for (let i = 0; i < data.weather.length; i++) {
-        
-                let forecastDay = document.createElement('article');
-                forecastDay.innerHTML = `<b>${forecastDays[i]}</b>`
-        
-                let avgTemp = document.createElement('p');
-                avgTemp.innerHTML = `<b>Average Temperature:</b> ${data.weather[i].avgtempF}°F`
-        
-                let maxTemp = document.createElement('p');
-                maxTemp.innerHTML = `<b>Max Temperature:</b> ${data.weather[i].maxtempF}°F`
-        
-                let minTemp = document.createElement('p');
-                minTemp.innerHTML = `<b>Min Temperature:</b> ${data.weather[i].mintempF}°F`
-        
-                forecastDay.append(avgTemp, maxTemp, minTemp);
-                document.querySelector('.forecast').append(forecastDay)
-        
-                // weatherToday.append(document.createElement('p').innerHTML = `<b>Average Temperature:</b> ${data.current_condition[0].FeelsLikeF}`)
-                // weatherToday.createElement('p').innerHTML = `<b>Max Temperature: ${data.weather}`
-            }
+
+    /**
+     * Function to display all weather data once FETCH'd
+     *  Nested within fetchWeather function for variable scpe reasons
+     */
+    function displayWeather(data) {
+        // Using object destructuring to pull data from json
+        let { value: nearest } = data.nearest_area[0].areaName[0];
+        let { value: region } = data.nearest_area[0].region[0];
+        let { value: country } = data.nearest_area[0].country[0];
+        let { FeelsLikeF } = data.current_condition[0];
+        let { chanceofsunshine } = data.weather[0].hourly[4];
+        let { chanceofrain } = data.weather[0].hourly[4];
+        let { chanceofsnow } = data.weather[0].hourly[4];
+
+        console.log(nearest, region, country, FeelsLikeF, chanceofsunshine, chanceofrain, chanceofsnow);
+
+        // Following lines take relevant info from json/data, and place into p-tags within main
+
+        // Changes weather icon based on chance of relevant weather
+        if (chanceofsunshine > 50) {
+            weatherIcon.src = './assets/icons8-summer.gif';
+            weatherIcon.alt = 'sun';
+        } else if (chanceofrain > 50) {
+            weatherIcon.src = './assets/icons8-torrential-rain.gif';
+            weatherIcon.alt = 'rain';
+        } else if (chanceofsnow > 50) {
+            weatherIcon.src = './assets/icons8-light-snow.gif';
+            weatherIcon.alt = 'snow';
+        }
+
+        // Edge case check of NEAREST value to switch between 'Area' & 'Nearest Area'
+        if (city.toLowerCase() === nearest.toLowerCase()) {
+            document.querySelector(".nearest").innerHTML = `<b>Area:</b> ` + nearest;
+        } else {
+            document.querySelector(".nearest").innerHTML = `<b>Nearest Area:</b> ` + nearest;
+        }
+        document.querySelector(".region").innerHTML = `<b>Region:</b> ` + region;
+        document.querySelector(".country").innerHTML = `<b>Country:</b> ` + country;
+        document.querySelector(".currentTemp").innerHTML = `<b>Currently:</b> Feels Like ` + FeelsLikeF + ` &degF`;
+        document.querySelector(".sunChance").innerHTML = `<b>Chance of Sunshine:</b> ` + chanceofsunshine + `%`;
+        document.querySelector(".rainChance").innerHTML = `<b>Chance of Rain:</b> ` + chanceofrain + `%`;
+        document.querySelector(".snowChance").innerHTML = `<b>Chance of Snow:</b> ` + chanceofsnow + `%`;
+
+
+        document.querySelector('.forecast').innerHTML = '';
+        const forecastDays = ['Today', 'Tomorrow', 'Day After Tomorrow'];
+
+        // Loop for Forecast
+        for (let i = 0; i < data.weather.length; i++) {
+
+            let forecastDay = document.createElement('article');
+            forecastDay.innerHTML = `<b>${forecastDays[i]}</b>`
+
+            let avgTemp = document.createElement('p');
+            avgTemp.innerHTML = `<b>Average Temperature:</b> ${data.weather[i].avgtempF}°F`
+
+            let maxTemp = document.createElement('p');
+            maxTemp.innerHTML = `<b>Max Temperature:</b> ${data.weather[i].maxtempF}°F`
+
+            let minTemp = document.createElement('p');
+            minTemp.innerHTML = `<b>Min Temperature:</b> ${data.weather[i].mintempF}°F`
+
+            forecastDay.append(avgTemp, maxTemp, minTemp);
+            document.querySelector('.forecast').append(forecastDay)
+
+            // weatherToday.append(document.createElement('p').innerHTML = `<b>Average Temperature:</b> ${data.current_condition[0].FeelsLikeF}`)
+            // weatherToday.createElement('p').innerHTML = `<b>Max Temperature: ${data.weather}`
         }
     }
+}
 
 
 // Temperature Conversion
