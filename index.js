65604fbc736cfadb3fbee9e3bd115ac3dd431aa9
Wilsantos1975 <Faustosantos@pursuit.org>
2023-01-09 11:49:18 -0500

    added css

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ const getWeatherInfo = (url, input) => {
 };
 
 const handleResponse = (response, input) => {
+
+  removeElement();
   const mainContainer = document.querySelector(".main");
   const mainData = populateMain(response, input);
   mainContainer.append(mainData);
@@ -26,10 +28,14 @@ const handleResponse = (response, input) => {
   const forecastContainer = document.querySelector(".forecast");
   const forecastArticles = populateForecast(response);
   forecastContainer.append(forecastArticles);
+
+  const previousContainer = document.querySelector(".previous");
+  const previousSearches = createPreviousSearch(response, input);
+  previousContainer.append(previousSearches);
 };
 
 const populateMain = (weather, input) => {
-  removeElement();
+  
   const mainDiv = document.createElement("div");
 
   const h1 = document.createElement("h1");
@@ -41,9 +47,7 @@ const populateMain = (weather, input) => {
   const country = document.createElement("p");
   country.innerHTML = `Country: ${weather.nearest_area[0].country[0].value}`;
   const currently = document.createElement("p");
-  currently.innerHTML = `Currently: ${weather.current_condition[0]["FeelsLikeF"]}`;
-
- 
+  currently.innerHTML = `Currently: ${weather.current_condition[0]["FeelsLikeF"]}°F`;
 
   mainDiv.append(h1, area, region, country, currently);
   return mainDiv;
@@ -58,39 +62,42 @@ const removeElement = () => {
 
 const populateForecast = (weather) => {
   const daysArticles = document.querySelectorAll(".day");
- 
+
   for (let i = 0; i < daysArticles.length; i++) {
     let daysForecast = ["Today", "Tomorrow", "Day After Tomorrow"];
     const h3 = document.createElement("h3");
     h3.innerHTML = daysForecast[i];
 
     const averageTemperature = document.createElement("section");
-    averageTemperature.innerHTML = `Average Temperature:${weather.weather[0].avgtempF}`;
+    averageTemperature.innerHTML = `Average Temperature:${weather.weather[0].avgtempF}°F`;
 
     const maxTemperature = document.createElement("section");
-    maxTemperature.innerHTML = `Max Temperature:${weather.weather[0].maxtempF}`;
+    maxTemperature.innerHTML = `Max Temperature:${weather.weather[0].maxtempF}°F`;
 
     const minTemperature = document.createElement("section");
-    minTemperature.innerHTML = `Min Temperature:${weather.weather[0].mintempF}`;
+    minTemperature.innerHTML = `Min Temperature:${weather.weather[0].mintempF}°F`;
 
-    daysArticles[i].append(h3,averageTemperature,maxTemperature,minTemperature) //append child didnt work. research more
-    // aside.append(daysArticles[i])
+    daysArticles[i].append(
+      h3,
+      averageTemperature,
+      maxTemperature,
+      minTemperature
+    ); //append child didnt work. research more
   }
-  
-  // return daysArticles
 };
-// const createPreviousSearch = (weather,input) => {
-// const li = document.createElement("a");
-// li.href = weather;
-// li.innerHTML = input;
+const createPreviousSearch = (weather, input) => {
+  let ul = document.querySelector(".previous")
+  const searchli = document.createElement("a");
+  searchli.href = 'www.google.com';
+  searchli.innerHTML = input;
 
-// li.addEventListener("click", liClickEvent)
-// const feelsLikeF = `${weather.current_condition[0]["FeelsLikeF"]}`
+  searchli.addEventListener("click", liClickEvent);
+  const feelsLikeTemp = `${weather.current_condition[0]["FeelsLikeF"]}°F`;
 
-// const searchliElement = document.createElement("li");
-// searchliElement.append(li, "-", feelsLikeF)
-// return searchliElement
-
-// }
+  const searchliElement = document.createElement("li");
+  searchliElement.append(searchli, "-", feelsLikeTemp);
+  ul.append(searchliElement)
+  
+};
 
 weatherFormSubmit.addEventListener("submit", weatherFormSubmitEvent);
