b4a994d44f76443bef437511fdcb84809bbe4e67
MaxWattan <mcwattanac@gmail.com>
2023-01-08 22:44:06 -0500

    phase 2

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ document.querySelector("form").addEventListener("submit", (event) => {
         console.log("Fetch was successful")
         return result.json()
         
+        
     }).then(weather => {
         document.querySelector("input").value = ""
         console.log(weather, "here")
@@ -24,14 +25,64 @@ document.querySelector("form").addEventListener("submit", (event) => {
         const p2 = document.createElement("p")
         const p3 = document.createElement("p")
         const p4 = document.createElement("p")
-        p1.textContent = `Area: ${weather.nearest_area[0].areaName[0].value}`
-        p2.textContent = `Region: ${weather.nearest_area[0].region[0].value}`
-        p3.textContent = `Country: ${weather.nearest_area[0].country[0].value}`
-        p4.textContent = `Currently: Feels like ${weather.current_condition[0].FeelsLikeF}°F`
+        p1.innerHTML = `<strong>Area:</strong> ${weather.nearest_area[0].areaName[0].value}`
+        p2.innerHTML = `<strong>Region:</strong> ${weather.nearest_area[0].region[0].value}`
+        p3.innerHTML = `<strong>Country:</strong> ${weather.nearest_area[0].country[0].value}`
+        p4.innerHTML = `<strong>Currently:</strong> Feels like ${weather.current_condition[0].FeelsLikeF}°F`
         
         newArticle.append(h2, p1, p2, p3, p4)
-       
-    })
 
     
+    
+    let today = document.querySelector('.today')
+    let tomorrow = document.querySelector('.tomorrow')
+    let day = document.querySelector('.day_after_tomorrow')
+
+    today.innerHTML = `<h1>Today</h1>`
+    let pToday = document.createElement('p')
+    
+    pToday.innerHTML =
+    `<span class="today"><strong>Average Temperature:</strong> ${weather.weather[0].avgtempF} &#8457</span>
+    <br>
+    <span class="today"><strong>Max Temperature:</strong> ${weather.weather[0].maxtempF} &#8457</span>
+    <br>
+    <span class="today"><strong>Min Temperature:</strong> ${weather.weather[0].mintempF} &#8457</span>`
+    today.append(pToday)
+    
+
+    tomorrow.innerHTML = `<h1>Tomorrow</h1>`
+    let pTom = document.createElement('p')
+    pTom.innerHTML =
+    `<span class="today"><strong>Average Tempature:</strong> ${weather.weather[1].avgtempF} &#8457</span>
+    <br>
+    <span class="today"><strong>Max Temperature:</strong> ${weather.weather[1].maxtempF} &#8457</span>
+    <br>
+    <span class="today"><strong>Min Temperature:</strong> ${weather.weather[1].mintempF} &#8457</span>`
+    tomorrow.append(pTom)
+
+    day.innerHTML = `<h1>Day After Tomorrow</h1>`
+    let dayAfter = document.createElement('p')
+    dayAfter.innerHTML =
+    `<span class="today"><strong>Average Tempature:</strong> ${weather.weather[2].avgtempF} &#8457</span>
+    <br>
+    <span class="today"><strong>Max Temperature:</strong> ${weather.weather[2].maxtempF} &#8457</span>
+    <br>
+    <span class="today"><strong>Min Temperature:</strong> ${weather.weather[2].mintempF} &#8457</span>`
+    day.append(dayAfter)
+       
+    
+})
 });
+const conversion = document.querySelector('#conversionForm');
+conversion.addEventListener('submit', (event) => {
+event.preventDefault();
+let conversion = event.target.convert.value;
+let toC = document.querySelector('#to-c')
+let toF = document.querySelector('#to-f')
+if (toC.checked) {
+conversion = (conversion - 32) * (5 / 9);
+document.querySelector('#result').innerHTML = `${conversion.toFixed(2)}°C`;
+ } else {
+conversion = conversion * (9 / 5) + 32;
+document.querySelector('#result').innerHTML = `${conversion.toFixed(2)}°F`;
+}});
