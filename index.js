f93c491d2571f04523a9077190146bd838253aa7
Wilsantos1975 <Faustosantos@pursuit.org>
2023-01-09 21:58:40 -0500

    debuuged nodelist issue and populated previous side

--- index.js
+++ index.js
@@ -23,34 +23,35 @@ const handleResponse = (response, input) => {
   const mainContainer = document.querySelector(".main");
   mainContainer.innerHTML = ""
   const mainData = populateMain(response, input);
-  mainContainer.append(mainData);
+  // mainContainer.append(mainData);
   
   const forecastContainer = document.querySelector(".forecast");
-  // forecastContainer.innerHTML = ""
   const forecastArticles = populateForecast(response);
-  forecastContainer.append(forecastArticles);
+  // forecastContainer.append(forecastArticles);
   
   const previousContainer = document.querySelector(".previous");
   const previousSearches = createPreviousSearch(response, input);
-  previousContainer.append(previousSearches);
+  previousContainer.append(...previousSearches);
 };
 
 const populateMain = (weather, input) => {
-  const mainDiv = document.createElement("div");
+  const mainArticle = document.querySelector(".main");
   
   const h1 = document.createElement("h1");
   h1.innerHTML = input;
   const area = document.createElement("p");
-  area.innerHTML = `Area: ${weather.nearest_area[0].areaName[0].value}`;
+  area.innerHTML = `<strong>Area:</strong> ${weather.nearest_area[0].areaName[0].value}`;
   const region = document.createElement("p");
-  region.innerHTML = `Region: ${weather.nearest_area[0].region[0].value}`;
+  region.innerHTML = `<strong>Region:</strong> ${weather.nearest_area[0].region[0].value}`;
   const country = document.createElement("p");
-  country.innerHTML = `Country: ${weather.nearest_area[0].country[0].value}`;
+  country.innerHTML = `<strong>Country:</strong> ${weather.nearest_area[0].country[0].value}`;
   const currently = document.createElement("p");
-  currently.innerHTML = `Currently: ${weather.current_condition[0]["FeelsLikeF"]}°F`;
+  currently.innerHTML = `<strong>Currently:</strong> Feels Like ${weather.current_condition[0].FeelsLikeF}°F`;
   
-  mainDiv.append(h1, area, region, country, currently);
-  return mainDiv;
+  mainArticle.append(h1, area, region, country,currently);
+  
+  console.log(weather)
+  
 };
 
 const removeElement = () => {
@@ -70,14 +71,16 @@ const populateForecast = (weather) => {
     const h3 = document.createElement("h3");
     h3.innerHTML = daysForecast[i];
 
+    daysArticles[i].style.background = "blue"
+
     const averageTemperature = document.createElement("section");
-    averageTemperature.innerHTML = `Average Temperature:${weather.weather[0].avgtempF}°F`;
+    averageTemperature.innerHTML = `<strong>Average Temperature:</strong> ${weather.weather[i].avgtempF}°F`;
 
     const maxTemperature = document.createElement("section");
-    maxTemperature.innerHTML = `Max Temperature:${weather.weather[0].maxtempF}°F`;
+    maxTemperature.innerHTML = `<strong>Max Temperature:</strong> ${weather.weather[i].maxtempF}°F`;
 
     const minTemperature = document.createElement("section");
-    minTemperature.innerHTML = `Min Temperature:${weather.weather[0].mintempF}°F`;
+    minTemperature.innerHTML = `<strong>Min Temperature:</strong> ${weather.weather[i].mintempF}°F`;
 
     daysArticles[i].append(
       h3,
@@ -86,19 +89,27 @@ const populateForecast = (weather) => {
       minTemperature
     ); //append child didnt work. research more
   }
+  console.log(daysArticles)
+  return daysArticles
 };
+
 const createPreviousSearch = (weather, input) => {
   let ul = document.querySelector(".previous");
-  const searchli = document.createElement("a");
-  searchli.href = "www.google.com";
-  searchli.innerHTML = input;
+  const searchAnchor = document.createElement("a");
+  // searchli.href = "#";
+  searchAnchor.innerHTML = input;
 
-  searchli.addEventListener("click", liClickEvent);
-  const feelsLikeTemp = `${weather.current_condition[0]["FeelsLikeF"]}°F`;
+  searchAnchor.addEventListener("click", liClickEvent);
+  const feelsLikeTemp = document.createElement("span");
+  feelsLikeTemp.innerHTML = ` - ${weather.current_condition[0].FeelsLikeF}°F` 
 
   const searchliElement = document.createElement("li");
-  searchliElement.append(searchli, "-", feelsLikeTemp);
+  searchliElement.append(searchAnchor,feelsLikeTemp);
   ul.append(searchliElement);
 };
 
+const liClickEvent = (event) => {
+event.preventDefault();
+
+}
 weatherFormSubmit.addEventListener("submit", weatherFormSubmitEvent);
