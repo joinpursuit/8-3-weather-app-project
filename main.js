44cfb35cee028dc6e04b724c2c83a8eed22ac55f
Diegocarrero <diegocar2413@gmail.com>
2023-01-18 16:33:36 -0500

    Added JS code to show the city name and area in the weather display after search

--- main.js
+++ main.js
@@ -6,8 +6,26 @@ document.querySelector("form").addEventListener("submit", (event) => {
     fetch(API_URL + event.target.location.value + query)
     .then(result => result.json())
     .then(weather => {
-        console.log(weather)
-        document.querySelector("article")
+        console.log(weather);
+        const location = event.target.location.value;
+        event.target.location.value = '';
+
+        const weatherDisplay = document.querySelector('main article');
+        weatherDisplay.innerHTML = '';
         // weather.current-condition.0.FeelsLikeF
+
+        const city = document.createElement('h2');
+        city.textContent = location;
+        weatherDisplay.append(city);
+
+        const mainSection = document.createElement('p');
+        const area = weather.nearest_area[0].areaName[0].value;
+
+        if (location == area) {
+            mainSection.innerHTML = `<strong>Area:</strong> ${area}`;
+        } else {
+            mainSection.innerHTML = `<strong>Nearest Area:</strong> ${area}`
+        }
+        weatherDisplay.append(mainSection);
     })
 });
