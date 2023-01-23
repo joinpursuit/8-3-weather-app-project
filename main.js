c3da524fe738c0750aa75a1002e518bd1f90013e
laura-williams-1 <laurawilliams@pursuit.org>
2023-01-22 22:00:24 -0500

    Weather App

--- main.js
+++ main.js
@@ -17,9 +17,12 @@ document.querySelector("form").addEventListener("submit", (event) => {
       //article for current search
       let current = document.getElementById("current");
 
+      let currentHeading = document.createElement("h2");
+      currentHeading.innerHTML = `<b>${weather.nearest_area[0].areaName[0].value}</b>`;
+
       //area for current search w. html
       let searchArea = document.createElement("p");
-
+      // searchArea with HTML
       searchArea.innerHTML = `<b>Area: </b>${weather.nearest_area[0].areaName[0].value}`;
 
       //create region element
@@ -27,98 +30,112 @@ document.querySelector("form").addEventListener("submit", (event) => {
       //region innerHTML
       searchRegion.innerHTML = `<b>Region: </b>${weather.nearest_area[0].region[0].value}`;
 
-      current.append(searchRegion);
-      searchRegion.prepend(searchArea);
-      // console.log(searchRegion);
-
+      // create country HTML
       let searchCounty = document.createElement("p");
       searchCounty.innerHTML = `<b>Country: </b>${weather.nearest_area[0].country[0].value}`;
 
+      //Create currently & currently inner html
       let searchCurrentF = document.createElement("p");
-
       searchCurrentF.innerHTML = `<b> Currently: </b> Feels Like ${weather.weather[2].mintempF}° F`;
-      searchRegion.append(searchCounty, searchCurrentF);
 
+      //joininga all elements in current article
+      current.append(
+        currentHeading,
+        searchArea,
+        searchRegion,
+        searchCounty,
+        searchCurrentF
+      );
+
+      //-----adding CHANCE OF SUN + RAIN + SNOW-----
+
+      //creating chance of sun element + adding text
+      let chanceOfSun = document.createElement("p");
+      chanceOfSun.innerHTML = `<b> Chance of Sunshine: </b>${weather.weather[0].hourly[0].chanceofsunshine}`;
+      console.log(chanceOfSun.innerHTML);
+      //creating chance of rain element + adding text
+      let chanceOfRain = document.createElement("p");
+      chanceOfRain.innerHTML = `<b> Chance of Rain: </b>${weather.weather[0].hourly[0].chanceofrain}`;
+
+      //creating chance of snow element + adding text
+      let chanceOfSnow = document.createElement("p");
+      chanceOfSnow.innerHTML = `<b> Chance of Snow: </b>${weather.weather[0].hourly[0].chanceofsnow}`;
+
+      //adding all those to first article,
+      current.append(chanceOfSun, chanceOfRain, chanceOfSnow);
+      //-------Aritcle for Today section ---
+
+      //gets article for today section
       let article = document.getElementById("today");
+      //adds an header for "today" + adding text
+      let articleHeader = document.createElement("h2");
+      articleHeader.innerHTML = `<b> Today </b>`;
 
+      // creating TodayAvgTemp + inner HTML
       let todayAvgTemp = document.createElement("p");
-
       todayAvgTemp.innerHTML = `<b> Average Temperature: </b> ${weather.weather[0].avgtempF}°F`;
 
+      // creating TodayMaxTemp + inner HTML
       let todayMaxTemp = document.createElement("p");
-
       todayMaxTemp.innerHTML = `<b> Max Temperature: </b> ${weather.weather[0].maxtempF}°F`;
 
+      // creating TodayMinTemp + inner HTML
       let todayMinTemp = document.createElement("p");
-
       todayMinTemp.innerHTML = `<b> Max Temperature: </b> ${weather.weather[0].mintempF}°F`;
 
-      article.append(todayAvgTemp, todayMaxTemp, todayMinTemp);
+      // joining all lelemnts for today section
+      article.append(articleHeader, todayAvgTemp, todayMaxTemp, todayMinTemp);
 
+      //--------------Article for TOMORROW-----------
+      //gets article for TOMORROW  section
       let tomorrow = document.getElementById("tomorrow");
 
+      //adds an header for "TOMORROW" + adding text
       let tomorrowHeader = document.createElement("h2");
-
       tomorrowHeader.innerHTML = `<b>Tomorrow</b>`;
-      article.append(tomorrowHeader);
-      let tmmAvgTemp = document.createElement("p");
 
+      // creating TomorrowAvgTemp + inner HTML
+      let tmmAvgTemp = document.createElement("p");
       tmmAvgTemp.innerHTML = `<b> Average Temperature: </b> ${weather.weather[1].avgtempF}°F`;
 
+      // creating TomorrowMaxTemp + inner HTML
       let tmmMaxTemp = document.createElement("p");
       tmmMaxTemp.innerHTML = `<b> Max Temperature: </b> ${weather.weather[1].maxtempF}°F`;
 
+      // creating TomorrowMinTemp + inner HTML
       let tmmMinTemp = document.createElement("p");
-
       tmmMinTemp.innerHTML = `<b> Max Temperature: </b> ${weather.weather[1].mintempF}°F`;
 
-      tomorrow.append(tmmAvgTemp, tmmMaxTemp, tmmMinTemp);
+      // joining all elements into TOMORROW'S article
+      tomorrow.append(tomorrowHeader, tmmAvgTemp, tmmMaxTemp, tmmMinTemp);
 
+      //--------------DAY AFTER TOMOROW-----------
+
+      //gets article for TOMORROW  section
+      let dayAfter = document.getElementById("day-after");
+      //adds an header for "day after" + adding text
+      let dayAferHeading = document.createElement("h2");
+      dayAferHeading.innerHTML = `<b>Day After Tomorrow</b>`;
+
+      // creating dayAfterAvgTemp + inner HTML
       let dayAfteryAvgTemp = document.createElement("p");
+      dayAfteryAvgTemp.innerHTML = `<b> Average Temperature: </b> ${weather.weather[2].avgtempF}°F`;
 
+      // creating dayAfterMaxTemp + inner HTML
       let dayAfterMaxTemp = document.createElement("p");
+      dayAfterMaxTemp.innerHTML = `<b> Max Temperature: </b> ${weather.weather[2].maxtempF}°F`;
 
+      // creating dayAfterMinTemp + inner HTML
       let dayAfterMinTemp = document.createElement("p");
-      // weather.current_condition[0].FeelsLikeC;
-      // //area of search
-      // console.log(weather.nearest_area[0].areaName[0].value);
-      // region of search
-      // console.log(weather.nearest_area[0].region[0].value);
-      // country of search
-      // console.log(weather.nearest_area[0].country[0].value);
-      // console.log(weather.weather[2].mintempF);
-
-      //aside
-      // console.log("today temp");
-      // //TODAY- avg temp
-      // console.log(weather.weather[0].avgtempF);
-
-      // today maX temo
-      // console.log(weather.weather[0].maxtempF);
-
-      //today min
-      // console.log(weather.weather[0].mintempF);
-
-      console.log("tomorrow");
-      //TM- avg temp
-      console.log(weather.weather[1].avgtempF);
-
-      //TM maX temo
-      console.log(weather.weather[1].maxtempF);
-
-      //TM min
-      console.log(weather.weather[1].mintempF);
-
-      console.log("day after tomorrow");
-      //TM- avg temp
-      console.log(weather.weather[2].avgtempF);
-
-      //TM maX temo
-      console.log(weather.weather[2].maxtempF);
-
-      //TM min
-      console.log(weather.weather[2].mintempF);
-      console.log("p");
+      dayAfterMinTemp.innerHTML = `<b> Max Temperature: </b> ${weather.weather[2].mintempF}°F`;
+      // joining all element into day after article
+      dayAfter.append(
+        dayAferHeading,
+        dayAfteryAvgTemp,
+        dayAfterMaxTemp,
+        dayAfterMinTemp
+      );
+      //------Creatung Aside ----
       console.log(weather, "Finally properly formatted weather");
     });
 });
