56b1e5ba0915beaab56e610b35f4a5b9e2663300
MichelBDLC <michelbdlc@gmail.com>
2023-01-24 03:53:05 -0800

    first 5 tests passing

--- main.js
+++ main.js
@@ -21,8 +21,9 @@ const datMinTemp = document.querySelector('.dat-min-temp');
 const weatherDataSummary = document.querySelector('.weather-days');
 const previousSect = document.querySelector('.previous');
 const previousSearch = document.querySelector('.previous-search');
-const noPreviousSearchMsg = document.querySelector('.no-search-msg');
 const previousList = document.querySelector('.previous-list');
+const noSearchMsgP = document.querySelector('.no-search-msg');
+const area = document.querySelector('.area');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -30,12 +31,56 @@ form.addEventListener('submit', (event) => {
     theInput = event.target.location.value; 
 
     const url = `https://wttr.in/` + theInput + `?format=j1`;
-
+    
     fetch(url)
     .then((response) => response.json())
     .then((data) => { 
 
-        console.log(data)
+        console.log(data);
+
+        defaultMsg.style.display = `none`; 
+        placementWeatherInfo.style.height = `270px`
+        weatherDataSummary.style.opacity = `100%`;
+        
+        cityName.textContent = theInput;
+        area.textContent = "Nearest Area: " + data.nearest_area[0].areaName[0].value;
+        region.textContent = "Region: " + data.nearest_area[0].region[0].value;
+        country.textContent = "Country: " + data.nearest_area[0].country[0].value;
+        currently.textContent = "Currently: " + "Feels like " + data.current_condition[0].FeelsLikeF + "°F";
+
+        todayAvgTemp.textContent = `Average Temperature: ${data.weather[0].avgtempF} °F`;
+        todayMaxTemp.textContent = `Max Temperature: ${data.weather[0].maxtempF} °F`;
+        todayMinTemp.textContent = `Min Temperature: ${data.weather[0].mintempF} °F`;
+
+        tmmAvgTemp.textContent = `Average Temperature: ${data.weather[1].avgtempF} °F`;
+        tmmMaxTemp.textContent = `Max Temperature: ${data.weather[1].maxtempF} °F`;
+        tmmMinTemp.textContent = `Min Temperature: ${data.weather[1].mintempF} °F`;
+
+        datAvgTemp.textContent = `Average Temperature: ${data.weather[2].avgtempF} °F`;
+        datMaxTemp.textContent = `Max Temperature: ${data.weather[2].maxtempF} °F`;
+        datMinTemp.textContent = `Min Temperature: ${data.weather[2].mintempF} °F`;
+
+        if (noSearchMsgP.textContent.length > 2) {
+            noSearchMsgP.remove();
+        }
+
+        const li = document.createElement('li'); 
+        previousList.appendChild(li);
+        li.style = `list-style: none`;
+        const a = document.createElement('a');
+        a.setAttribute('href', '#');
+        li.appendChild(a);
+        a.textContent = data.nearest_area[0].areaName[0].value + ' ' + data.current_condition[0].FeelsLikeF + "°F";
+
+        a.addEventListener('click', (event) => {
+           
+            theInput = event.target.textContent.split(' ')[0];
+
+            cityName.textContent = theInput;
+
+            fetch(url)
+            .then((response) => response.json())
+            .then((data) => { 
 
         defaultMsg.style.display = `none`; 
         placementWeatherInfo.style.height = `270px`
@@ -57,28 +102,17 @@ form.addEventListener('submit', (event) => {
         datAvgTemp.textContent = `Average Temperature: ${data.weather[2].avgtempF} °F`;
         datMaxTemp.textContent = `Max Temperature: ${data.weather[2].maxtempF} °F`;
         datMinTemp.textContent = `Min Temperature: ${data.weather[2].mintempF} °F`;
-
-        noPreviousSearchMsg.style.display = `none`;
-        const li = document.createElement('li');
-        previousList.appendChild(li);
-        const a = document.createElement('a');
-        li.appendChild(a);
-        a.textContent = cityName + data.current_condition[0].FeelsLikeF + "°F";
-
-        // a.addEventListener('click', (event) => {
-
-        // })
-
-
-
-
-
-          
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+        })         
     })
     .catch((error) => {
         console.log(error)
     })
+    form.reset();
 })
 
-//adds each search to a list in the previous section 
-//add event listener, if clicked on, show info of the name of the city that it contains!
+//if previos search includes cityname do not record
+//check that all these variables are indeed being used when done with command find
